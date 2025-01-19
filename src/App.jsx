bbef4f27cd0e33a6b3d03cd7a9bd03a581feef92
BikramGhart1 <coludingrain@gmail.com>
2025-01-19 14:45:03 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Form from './components/Form'
+
+function App() {
+
+  return (
+    <div>
+           <div className="draggable">
+            
+           </div>
+           <div className="droppable">
+
+           </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Fullname.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Fullname() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default React.memo(Fullname)
